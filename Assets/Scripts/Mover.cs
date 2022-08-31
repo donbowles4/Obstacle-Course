09a405ce509efb7a7a472e8ffb43a091929cd105
Donald Bowles <donniebowles84@gmail.com>
2022-08-31 16:59:27 -0400

    Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,13 +7,24 @@ public class Mover : MonoBehaviour
     [SerializeField] float fltMoveSpeed = 10.0f;
     void Start()
     {
-        
+        PrintInstruction();    
     }
 
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
         float fltXValue = Input.GetAxis("Horizontal") * Time.deltaTime * fltMoveSpeed;
         float fltZValue = Input.GetAxis("Vertical") * Time.deltaTime * fltMoveSpeed;
         transform.Translate(fltXValue, 0, fltZValue);
+    }
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Use WASD or Arrow Keys to Move");
+        Debug.Log("Don't hit the walls");
     }
 }
