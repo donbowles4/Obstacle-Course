3908a342025fc8ff45658e60c971eff592d91aec
Donald Bowles <donniebowles84@gmail.com>
2022-09-05 13:17:41 -0400

    Time.time

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log(Time.time);
+    }
+}
